4a8ef11672676b50466b5e151926350779878547
Itsaadarsh <heyitsaadarsh@gmail.com>
2021-08-16 11:38:56 +0530

    Intersection of arrays

--- Array/UnionOfArrays.java
+++ Array/UnionOfArrays.java
@@ -6,9 +6,9 @@ import java.util.Set;
 
 public class UnionOfArrays {
     public static void main(String[] args) {
-        var a = new int[] { 85, 25, 1, 32, 54, 6 };
-        var b = new int[] { 85, 2 };
-        var op = union(a, a.length, b, b.length);
+        var a = new int[] { 1, 3, 2, 4, 8, 9, 0, 7, 7 };
+        var b = new int[] { 1, 3, 7, 5, 4, 0, 7, 5 };
+        var op = intersection(a, a.length, b, b.length);
         System.out.println(op);
     }
 
@@ -21,5 +21,19 @@ public class UnionOfArrays {
             listSet.add(Integer.valueOf(bNum));
         }
         return listSet.size();
+    }
+
+    static int intersection(int a[], int n, int b[], int m) {
+        Set<Integer> listSetA = new HashSet<Integer>();
+        Set<Integer> listSetB = new HashSet<Integer>();
+        for (int aNum : a) {
+            listSetA.add(Integer.valueOf(aNum));
+        }
+        for (int bNum : b) {
+            listSetB.add(Integer.valueOf(bNum));
+        }
+        listSetA.retainAll(listSetB);
+        return listSetA.size();
+
     }
 }
